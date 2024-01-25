5e2cbd3886daabd965fc8de924e6b76feef89f9c
Dilip Kumar <dilipkumer092@gmail.com>
2024-01-25 14:00:24 +0530

    change openAI as default

--- Services/OpenAI.py
+++ Services/OpenAI.py
@@ -1,28 +1,13 @@
 import google.generativeai as genai
 import openai
 from Services.ListenAndSpeak import Speak
-from Services.api_key import api_key
+from Services.api_key import api_key1, api_key2
 genai.configure(api_key = api_key1)
 
 def AI(query):
     try:
-        generation_config = {
-        "temperature": 0.9,
-        "top_p": 1,
-        "top_k": 1,
-        "max_output_tokens": 512,
-        }
-
-        model = genai.GenerativeModel(model_name="gemini-pro",
-                                    generation_config=generation_config)
-        
-        convo = model.start_chat()
-        convo.send_message(query)
-        resp = (convo.last.text).replace('*', ' ')
-        Speak(resp)
-    except:
         chatStr = ""
-        openai.api_key = apikey2
+        openai.api_key = api_key2
         chatStr += f"Intelligent Assistant:"
         response = openai.Completion.create(
         model="gpt-3.5-turbo-instruct",
@@ -36,7 +21,22 @@ def AI(query):
         chatStr += f"{response['choices'][0]['text']}\n"
         chatStr = ""
         Speak(response["choices"][0]["text"])
-
         return response["choices"][0]["text"]
+        
+    except:
+        generation_config = {
+        "temperature": 0.9,
+        "top_p": 1,
+        "top_k": 1,
+        "max_output_tokens": 512,
+        }
+        model = genai.GenerativeModel(model_name="gemini-pro",
+                                    generation_config=generation_config)
+        
+        convo = model.start_chat()
+        convo.send_message(query)
+        resp = (convo.last.text).replace('*', ' ')
+        Speak(resp)
+        
 
 
